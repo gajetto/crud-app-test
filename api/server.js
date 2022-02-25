d3517113ffbe78be13e7c0b95031dcac58c9a21a
llabbe <gajettokeibu@gmail.com>
2022-02-25 15:10:03 +0100

    adding ENV variables to DB

--- api/server.js
+++ api/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const env = require('dotenv').config();
 const bodyParser = require('body-parser');
 const PORT = 4000;
 const cors = require('cors');
@@ -7,24 +8,27 @@ const mongoose = require('mongoose');
 //const config = require('./DB.js');
 const employerRoute = require('./employer.route');
 const Employer = require('./employer.model')
-//const DbURI = "mongodb://localhost:27017/test";
 
 mongoose.Promise = global.Promise;
-// locahost connection
-//mongoose.connect(DbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }).then(
-//  () => {console.log('Database is connected') },
-//  err => { console.log('Can not connect to the database'+ err)}
-//);
+
+let password =  process.env.DB_PASSWORD;
+let user = process.env.DB_USER;
+
 
 mongoose.connect('mongodb+srv://cluster0.xi2k0.mongodb.net/react-db?retryWrites=true&w=majority',
 {
-  user: 'gajetto',
-  pass: 'Hjh65gfds*Uds',
+  //user: process.env.DB_USER,
+  //pass: process.env.DB_PASSWORD,
+  user: user,
+  pass: password,
   useNewUrlParser: true, 
   useUnifiedTopology: true, 
   useCreateIndex: true
 }
-  )
+  ).then(
+  () => {console.log('Database is connected') },
+  err => { console.log('Can not connect to the database'+ err)}
+);
   
 
 app.use(cors());  
@@ -36,18 +40,3 @@ app.use('/employer', employerRoute);
 app.listen(PORT, function(){
   console.log('Server is running on Port:',PORT);
 });
-
-//app.get('/add', (req, res) => {
-//  const employee = new Employer({
-//    person_name:"Peter Parcoeur",
-//    employer_name:"Marvel and Ninjas Ink Corportation",
-//    employer_id_number:20
-//  });
-//  employee.save()
-//    .then((result) => {
-//      res.send(result)
-//    })
-//    .catch((err) => {
-//      console.log(err);
-//    });
-//})
