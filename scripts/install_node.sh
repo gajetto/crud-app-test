4f5c78fc246ebd4c52040506e241bfd89353f70d
llabbe <gajettokeibu@gmail.com>
2022-03-10 19:27:02 +0100

    daemon script forever added and pub ip address replacement

--- scripts/install_node.sh
+++ scripts/install_node.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-cd /var/www
-sudo npm install -g npm@8.5.3
+cd /var/www/
+npm install -g npm@8.5.3
 npm install
 
+pwd
+
+cd api
 # install forever module for service availaibility
-sudo npm install -g --save forever 
+npm install -g --save forever 
 
 # Install dependencies on react front
-pwd
-cd api
+
 pwd
 npm install
 
@@ -17,12 +19,13 @@ echo -e "SKIP_PREFLIGHT_CHECK=true
 DB_PASSWORD=admin123456 
 DB_USER=adminReact 
 DB_NAME=mongodb://10.0.1.50/react-db?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.2.
-DEBUGLEVEL=5" > /.env
+DEBUGLEVEL=5" > .env
 
 
 
 cd ../src/components
 pwd
+
 IPADDRESS=$(curl http://checkip.amazonaws.com)
 
-sudo sed -i 's|localhost|$IPADDRESS|g' TableRow.js index.component.js create.component.js edit.component.js
+sudo sed -i 's|localhost|'$IPADDRESS'|g' TableRow.js index.component.js create.component.js edit.component.js
