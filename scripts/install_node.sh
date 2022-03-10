d398ec84abcd66daa0b58818390d1e23470204cc
llabbe <gajettokeibu@gmail.com>
2022-03-10 14:34:22 +0100

    only CD pipeline for faster checkin/testing and change root source directory in appspec file

--- scripts/install_node.sh
+++ scripts/install_node.sh
@@ -8,7 +8,9 @@ npm install
 sudo npm install -g --save forever 
 
 # Install dependencies on react front
+pwd
 cd api
+pwd
 npm install
 
 echo -e "SKIP_PREFLIGHT_CHECK=true
@@ -17,8 +19,10 @@ DB_USER=adminReact
 DB_NAME=mongodb://10.0.1.50/react-db?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.2.
 DEBUGLEVEL=5" > /.env
 
-cd ../src/components
 
+
+cd ../src/components
+pwd
 IPADDRESS=$(curl http://checkip.amazonaws.com)
 
 sudo sed -i 's|localhost|$IPADDRESS|g' TableRow.js index.component.js create.component.js edit.component.js
