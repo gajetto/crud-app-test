41136bea1781aa13fe2ba10d49c5d2e243fa5008
Ludovic Labbé <ludovic.labbe@elca.ch>
2022-04-22 15:16:23 +0200

    test azure boards number 1

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,5 @@
 # output "instance_public_ip" {
 #   description = "Public IP address of the EC2 instance"
-#   value       = aws_instance.web_server.public_ip
+#   value       = aws_instance.web_server.public_ip 
+# ADDDING stuff randomly TEST GIT PUSH TO GITHUB
 # }
